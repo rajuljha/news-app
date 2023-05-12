943b15de044727bd715d7c991bfab2d2a0484e9a
Rajul Jha <rajuljha49@gmail.com>
2023-05-12 17:07:20 +0530

    Add IP Address

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = ['localhost' , '127.0.0.1']
+ALLOWED_HOSTS = ['localhost' , '127.0.0.1', '64.227.153.119']
 
 
 # Application definition
